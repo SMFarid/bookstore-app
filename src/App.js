2cdcb0d28d3effb93264ce386cf1b5e3d40d082c
SMFarid <60547678+SMFarid@users.noreply.github.com>
2020-08-09 17:45:17 +0200

    Adding project directory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <a href="index.html">BookStore</a>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign in</a>
+          </div>
+        </header>
+
+        <main className="main">
+          <div className="content">
+            <ul className="products">
+              <li>
+                <div className="product">
+                  <img
+                    className="product-image"
+                    src="/images/book.png"
+                    alt="book"
+                  />
+                  <div className="title">
+                    <a href="product.html">Title</a>
+                  </div>
+                  <div className="author">Author</div>
+                  <div className="rating">Rating</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </main>
+
+        <footer className="footer">
+          <div>
+            Simple bookstore developed using React and Node. Icons made by
+            <a href="http://www.freepik.com/" title="Freepik">
+              Freepik
+            </a>{" "}
+            from
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </div>
+          <br />
+        </footer>
+      </div>
     </div>
   );
 }
